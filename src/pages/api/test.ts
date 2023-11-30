19da9ad7cac74de346e528ec34aec0efb3eec341
Evan Viguié <evan.viguie@proton.me>
2023-11-30 10:22:52 +0100

    Updated the 'astro.config.mjs' file by adding an output configuration. Created a new file 'test.ts' within the API folder, establishing GET and POST functions. Enabled prerendering on the Index page.

---
+++ src/pages/api/test.ts
@@ -0,0 +1,14 @@
+export async function GET() {
+    return new Response(
+        JSON.stringify({msg: "Hello world"}), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    );
+}
+
+export async function POST() {
+    return new Response()
+}
